5fc2c7e33903dcab4d2f3c0465f9581d891a9901
Bliss <andrej.kozhan00@mail.ru>
2021-10-19 11:38:06 +0300

    Добавление доп.ссылок на сайт

--- Controllers/CarsController.cs
+++ Controllers/CarsController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OnlineShop.Data.Interfaces;
+using OnlineShop.Data.Models;
 using OnlineShop.ViewModels;
 using System;
-
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OnlineShop.Data.Controllers
 {
@@ -16,13 +19,41 @@ namespace OnlineShop.Data.Controllers
             _allCars = iAllCars;
             _allCategories = iCarsCat;
         }
-        public ViewResult List()
+
+        [Route("Cars/List")]
+        [Route("Cars/List/{category}")]
+        public ViewResult List(string category)
         {
+            string _category = category;
+            IEnumerable<Car> cars = null;
+            string currCategory = "";
+            if (string.IsNullOrEmpty(category))
+            {
+                cars = _allCars.Cars.OrderBy(i => i.id);
+            }else
+            {
+                if(string.Equals("electro",category,StringComparison.OrdinalIgnoreCase))
+                {
+                    cars = _allCars.Cars.Where(i => i.category.categoryName.Equals("Электромобили")).OrderBy(i => i.id);
+                    currCategory = "Электромобили";
+                }
+                else if (string.Equals("fuel", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    cars = _allCars.Cars.Where(i => i.category.categoryName.Equals("Классические автомобили")).OrderBy(i => i.id);
+                    currCategory = "Бензиновые автомобили";
+                }
+   
+            }
+            var carObj = new CarsListViewModel
+            {
+                AllCars = cars,
+                currCategory = currCategory
+            };
+
+
             ViewBag.Title = "Онлайн магазин автомобилей";
-            CarsListViewModel obj = new CarsListViewModel();
-            obj.AllCars = _allCars.Cars;
-            obj.currCategory = "Новейшие автомобили";
-            return View(obj);
+           
+            return View(carObj);
         }
     }
 }
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data.Interfaces;
+using OnlineShop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllCars _carRep;       
+
+        public HomeController(IAllCars carRep)
+        {
+            _carRep = carRep;         
+        }
+
+        public ViewResult Index()
+        {
+            var homeCars = new HomeViewModel
+            {
+                favCars = _carRep.getFavouriteCars
+            };
+            return View(homeCars);
+        }
+    }
+}
